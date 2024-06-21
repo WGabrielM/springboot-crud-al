707ff35a92174712a7c7567301ea0427f99af861
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-06-20 22:39:39 -0300

    add DELETE method with logic disk not fisic

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -26,7 +26,7 @@ public class DoctorController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @PutMapping
@@ -39,6 +39,7 @@ public class DoctorController {
     @Transactional
     @DeleteMapping("/{id}")
     public void excluir(@PathVariable Long id) {
-        repository.deleteById(id);
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 }
